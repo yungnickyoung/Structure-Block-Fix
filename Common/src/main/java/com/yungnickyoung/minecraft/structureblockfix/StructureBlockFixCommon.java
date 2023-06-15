ce79612561b969a38c697013495fd1090e931798
Nick Young <yungnickyoung@gmail.com>
2023-06-14 21:59:04 -0400

    Initial 1.20 porting progress.

--- Common/src/main/java/com/yungnickyoung/minecraft/structureblockfix/StructureBlockFixCommon.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/structureblockfix/StructureBlockFixCommon.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 
 public class StructureBlockFixCommon {
     public static final String MOD_ID = "structureblockfix";
-    public static final String MOD_NAME = "StructureBlockFix";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public static void init() {
